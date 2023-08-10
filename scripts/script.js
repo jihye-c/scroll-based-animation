4e61c34765702c754c0dba3c12296b23f2ba4899
jihye-c <jihyec22@gmail.com>
2023-08-10 23:30:47 +0900

    fix text animation skip typo

--- scripts/script.js
+++ scripts/script.js
@@ -23,34 +23,27 @@ function conMeasure(){
 }
 conMeasure();
 
-const textAni = {
-    init(){
-        this.splitText();
-    },
-    splitText(){
-        const textBox = document.querySelector('.page1 .text');
-        const originalText = '스크롤에\n따라\n한 글자씩\n나타나는\n인터렉션\n효과';
-        const originTextArr = Array.from(originalText);
-        let textArr = [];
+function splitText(){
+    const textBox = document.querySelector('.page1 .text');
+    const originalText = '스크롤에\n따라\n한 글자씩\n나타나는\n인터렉션\n효과';
+    const originTextArr = Array.from(originalText);
+    let textArr = [];
 
-        function transferTextArr(arr, originVal, changeVal){
-            let idx;
-            while(idx !== -1){
-                idx = arr.indexOf(originVal);
-                arr[idx] = changeVal;
-            }
-            return arr;
+    function transferTextArr(arr, originVal, changeVal){
+        let idx;
+        while(idx !== -1){
+            idx = arr.indexOf(originVal);
+            arr[idx] = changeVal;
         }
-        textArr = transferTextArr(originTextArr, '\n', '<br/>');
-        for(let i = 0; i<originTextArr.length; i++){
-                textBox.innerHTML += `<span class='tbox'>${originTextArr[i]}<span>`;
-        }
-
-        container[1].divisionHeight = (container[1].height-1000) / textArr.length;
-
+        return arr;
     }
-};
-textAni.init();
+    textArr = transferTextArr(originTextArr, '\n', '<br/>');
+    for(let i = 0; i<=originTextArr.length; i++){
+            textBox.innerHTML += `<span class='tbox'>${originTextArr[i]}<span>`;
+    }
+    container[1].divisionHeight = (container[1].height-1000) / textArr.length;    
+}
+splitText();
 
 //scroll event
 const tbox = document.querySelectorAll('span.tbox');
@@ -59,17 +52,26 @@ let direction = '';
 document.addEventListener('scroll',(e)=>{
     let nowST = window.scrollY;
     const targetIndex = Math.round((nowST - container[1].top) / container[1].divisionHeight);
+    if(nowST > container[1].bot) return;
+    // console.log(`targetIndex = ${targetIndex}`);
     if(lastST < nowST){
         direction = 'down'
-        
-        if(targetIndex > -1 && targetIndex < 26){
-            tbox[targetIndex].style.opacity = '1';
+        if(targetIndex > -1 && targetIndex <= 25){
+            for(let i = 0; i <= targetIndex; i++){
+                tbox[i].style.opacity = '1';
+            }
+        }else if(targetIndex >= 26){
+            for(let i = 0; i < 26; i++){
+                tbox[i].style.opacity = '1';
+            }
         }
     }
     else{
         direction = 'up'
-        if(targetIndex > -1 && targetIndex < 26){
-            tbox[targetIndex].style.opacity = '0';
+        if(targetIndex > -1 && targetIndex <= 25){
+            for(let i = 26; i >= targetIndex; i--){
+                tbox[i].style.opacity = '0'
+            }
         }
     }
     lastST = nowST <= 0 ? 0 : nowST;
